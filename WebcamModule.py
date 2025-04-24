9a48983519e68146a8bee5402f2c28ee186a5711
1016468 <pl1016468@ahschool.com>
2025-04-24 09:36:44 -0400

    Update WebcamModule.py

--- WebcamModule.py
+++ WebcamModule.py
@@ -1,9 +1,8 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
-
 
 def getImg(display=False, size=[480, 240]):
+    cap = cv2.VideoCapture(0)
     _, img = cap.read()
     img = cv2.resize(img, (size[0], size[1]))
     if display:
